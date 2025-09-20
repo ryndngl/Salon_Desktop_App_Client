fbef29467fd49e7236769ac4dedf6d67d931f75d
Ryan Laurence Dingle <ryanlaurencedingle@gmail.com>
2025-09-20 23:44:51 +0800

    Update Loyalty Points page

--- src/pages/dashboard/LoyaltyPointsPage.jsx
+++ src/pages/dashboard/LoyaltyPointsPage.jsx
@@ -1,7 +1,7 @@
 const LoyaltyPointsPage = () => (
   <div className="space-y-6">
     <div className="bg-white rounded-lg shadow-md p-6 ml-auto">
-      <p className="text-gray-600">Loyalty points management functionality will be implemented here.</p>
+      <p className="text-gray-600">Loyalty points management functionality will be implemented here. coming soon</p>
     </div>
   </div>
 );
