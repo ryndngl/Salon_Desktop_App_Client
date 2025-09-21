97585b769f8cf1418a2a4745b5b189487343f887
Ryan Laurence Dingle <ryanlaurencedingle@gmail.com>
2025-09-22 02:17:11 +0800

    removed unecessary asterisk

--- src/pages/dashboard/WalkInComponents/WalkInForm.jsx
+++ src/pages/dashboard/WalkInComponents/WalkInForm.jsx
@@ -28,7 +28,7 @@ const WalkInForm = ({
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
           <div>
             <label className="block text-sm font-medium text-gray-700 mb-1">
-              Name *
+              Name 
             </label>
             <input
               type="text"
