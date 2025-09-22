912af87a1395e7b1c00cb61b7c1175a7283932be
Ryan Laurence Dingle <ryanlaurencedingle@gmail.com>
2025-09-23 04:14:02 +0800

    Add AppointmentComponents folder with barrel exports

---
+++ src/pages/dashboard/AppointmentComponents/index.js
@@ -0,0 +1,3 @@
+export { default as AppointmentsPage } from './AppointmentsPage.jsx';
+export { default as AppointmentStats } from './AppointmentStats.jsx';
+export { default as Appointmenttable } from './AppointmentTable.jsx';
