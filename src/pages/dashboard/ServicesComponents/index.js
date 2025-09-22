93b39883e293dda9991e1af16237f72df500a006
Ryan Laurence Dingle <ryanlaurencedingle@gmail.com>
2025-09-23 04:15:38 +0800

    Add ServicesComponents folder with barrel exports

---
+++ src/pages/dashboard/ServicesComponents/index.js
@@ -0,0 +1,6 @@
+export { default as ServicesPage } from './ServicesPage.jsx';
+export { default as ServicesStats } from './ServicesStats.jsx';
+export { default as ServicesGrid } from './ServicesGrid.jsx';
+export { default as ServiceCard } from './ServiceCard.jsx';
+export { default as ServiceCategory } from './ServiceCategory.jsx';
+export { default as ServiceStylesList } from './ServiceStylesList.jsx';
