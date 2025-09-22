a8d9c7e32b86e193a500666cdaad2ef23f55b7c8
Ryan Laurence Dingle <ryanlaurencedingle@gmail.com>
2025-09-23 04:16:32 +0800

    Add WalkInComponents folder with barrel exports

---
+++ src/pages/dashboard/WalkInComponents/index.js
@@ -0,0 +1,6 @@
+// WalkInComponents/index.js
+export { default as WalkInClientsPage } from './WalkInClientsPage.jsx';
+export { default as ServiceSelector } from './ServiceSelector.jsx';
+export { default as WalkInForm } from './WalkInForm.jsx';
+export { default as WalkInStats } from './WalkInStats.jsx';
+export { default as WalkInTable } from './WalkInTable.jsx';
