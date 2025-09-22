0bdf3284f8815e5f153b17484639d8801844d7df
Ryan Laurence Dingle <ryanlaurencedingle@gmail.com>
2025-09-23 04:13:31 +0800

    Remove old component files from previous locations

--- src/pages/dashboard/SalesReportPage.jsx
+++
@@ -1,9 +0,0 @@
-const SalesReportPage = () => (
-  <div className="space-y-6">
-    <div className="bg-white rounded-lg shadow-md p-6 ml-auto">
-      <p className="text-gray-600">Sales reports and analytics functionality will be implemented here.</p>
-    </div>
-  </div>
-);
-
-export default SalesReportPage;
--- src/pages/dashboard/ServicesPage.jsx
+++
@@ -1,7 +0,0 @@
-import ServicesMainComponents from './ServicesComponents/ServicesMainComponent'
-
-const ServicesPage = () => {
-  return <ServicesMainComponents />;
-};
-
-export default ServicesPage;
