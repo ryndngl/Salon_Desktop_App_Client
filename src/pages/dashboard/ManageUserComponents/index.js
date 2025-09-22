17c98263e19c3c06524cf8c729a8090bfa3f23bf
Ryan Laurence Dingle <ryanlaurencedingle@gmail.com>
2025-09-23 04:14:40 +0800

    Add ManageUserComponents folder with barrel exports

---
+++ src/pages/dashboard/ManageUserComponents/index.js
@@ -0,0 +1,7 @@
+export { default as ManageUserPage } from './ManageUserPage.jsx';
+export { default as UserDeleteModal } from './UserDeleteModal.jsx';
+export { default as UserDetailsModal } from './UserDetailsModal.jsx';
+export { default as UserEditModal } from './UserEditModal.jsx';
+export { default as UserSearchBar } from './UserSearchBar.jsx';
+export { default as UserStatsCard } from './UserStatsCard.jsx';
+export { default as UserTable } from './UserTable.jsx';
