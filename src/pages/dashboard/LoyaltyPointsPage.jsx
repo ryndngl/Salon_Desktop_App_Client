13c240747ce612cd6f870084b7f3f60792a4d7b3
Ryan Laurence Dingle <ryanlaurencedingle@gmail.com>
2025-09-27 01:08:20 +0800

    refactor: Remove old LoyaltyPointsPage from pages/dashboard

--- src/pages/dashboard/LoyaltyPointsPage.jsx
+++
@@ -1,9 +0,0 @@
-const LoyaltyPointsPage = () => (
-  <div className="space-y-6">
-    <div className="bg-white rounded-lg shadow-md p-6 ml-auto">
-      <p className="text-gray-600">Loyalty points management functionality will be implemented here. coming soon!</p>
-    </div>
-  </div>
-);
-
-export default LoyaltyPointsPage;
