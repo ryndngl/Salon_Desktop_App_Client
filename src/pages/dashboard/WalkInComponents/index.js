b60cc0b557fee7b271d7f8ffe3148fc1215951da
Ryan Laurence Dingle <ryanlaurencedingle@gmail.com>
2025-10-07 17:58:37 +0800

    refactor: add barrel export for WalkInComponents

--- src/pages/dashboard/WalkInComponents/index.js
+++ src/pages/dashboard/WalkInComponents/index.js
@@ -1,6 +1,7 @@
 // WalkInComponents/index.js
-export { default as WalkInClientsPage } from './WalkInClientsPage.jsx';
-export { default as ServiceSelector } from './ServiceSelector.jsx';
-export { default as WalkInForm } from './WalkInForm.jsx';
-export { default as WalkInStats } from './WalkInStats.jsx';
-export { default as WalkInTable } from './WalkInTable.jsx';
+export { default as WalkInClientsPage } from './WalkInClientsPage';
+export { default as WalkInTable } from './WalkInTable';
+export { default as WalkInStats } from './WalkInStats';
+export { default as WalkInForm } from './WalkInForm';
+export { default as WalkInFilters } from './WalkInFilters';
+export { default as ServiceSelector } from './ServiceSelector';
