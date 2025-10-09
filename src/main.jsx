3182ce6b829902be30b13e01591fb0f101016533
Ryan Laurence Dingle <ryanlaurencedingle@gmail.com>
2025-10-10 01:24:58 +0800

    feat(app): add AuthProvider wrapper in main.jsx for global auth state

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { LoginPage } from './pages/auth/AuthComponents';
+import App from './App'
+import { AuthProvider } from './hooks/useAuthState'
 import './index.css'
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LoginPage />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
 )
