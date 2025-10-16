f082946a4348420249f9c49699b4d14c2b55b78a
Ryan Laurence Dingle <ryanlaurencedingle@gmail.com>
2025-10-17 04:14:24 +0800

    Update export barrel in index.js

--- src/pages/dashboard/ServicesComponents/index.js
+++ src/pages/dashboard/ServicesComponents/index.js
@@ -1,6 +1,11 @@
 export { default as ServicesPage } from './ServicesPage.jsx';
 export { default as ServicesStats } from './ServicesStats.jsx';
 export { default as ServicesGrid } from './ServicesGrid.jsx';
-export { default as ServiceCard } from './ServiceCard.jsx';
-export { default as ServiceCategory } from './ServiceCategory.jsx';
+//export { default as ServiceCard } from './ServiceCard.jsx';
+//export { default as ServiceCategory } from './ServiceCategory.jsx';
 export { default as ServiceStylesList } from './ServiceStylesList.jsx';
+
+// ADD THESE:
+export { default as StyleCard } from './StyleCard.jsx';
+export { default as EditServiceModal } from './EditServiceModal.jsx';
+export { default as ToggleServiceModal } from './ToggleServiceModal.jsx';
