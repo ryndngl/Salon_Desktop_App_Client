7cbb9a313fa4bf0d08ddec6b5f9331e8f4d3f0f6
Ryan Laurence Dingle <ryanlaurencedingle@gmail.com>
2025-10-24 01:14:39 +0800

    chore: Update barrel exports for new modal components

--- src/pages/dashboard/WalkInComponents/index.js
+++ src/pages/dashboard/WalkInComponents/index.js
@@ -1,7 +1,11 @@
-// WalkInComponents/index.js
 export { default as WalkInClientsPage } from './WalkInClientsPage';
 export { default as WalkInTable } from './WalkInTable';
 export { default as WalkInStats } from './WalkInStats';
 export { default as WalkInForm } from './WalkInForm';
 export { default as WalkInFilters } from './WalkInFilters';
 export { default as ServiceSelector } from './ServiceSelector';
+
+// 🆕 New components
+export { default as ClientTypeModal } from './ClientTypeModal';
+export { default as ReturningClientSearchModal } from './ReturningClientSearchModal';
+export { default as VisitHistoryModal } from './VisitHistoryModal';
