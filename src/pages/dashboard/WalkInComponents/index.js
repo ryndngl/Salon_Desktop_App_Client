d8cb98b368bd96662173ac4baea90a974f407c66
Ryan Laurence Dingle <ryanlaurencedingle@gmail.com>
2025-10-25 02:18:43 +0800

    Update export barrel of WalkIn Components and delete ClientTypeMomdal file

--- src/pages/dashboard/WalkInComponents/index.js
+++ src/pages/dashboard/WalkInComponents/index.js
@@ -4,8 +4,5 @@ export { default as WalkInStats } from './WalkInStats';
 export { default as WalkInForm } from './WalkInForm';
 export { default as WalkInFilters } from './WalkInFilters';
 export { default as ServiceSelector } from './ServiceSelector';
-
-// 🆕 New components
-export { default as ClientTypeModal } from './ClientTypeModal';
 export { default as ReturningClientSearchModal } from './ReturningClientSearchModal';
 export { default as VisitHistoryModal } from './VisitHistoryModal';
